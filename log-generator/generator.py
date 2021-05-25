9983fedf489686bddb20ce63b9b511e6640b9174
Alexander Matsievsky <81968622+amatsievskiy-griddynamics@users.noreply.github.com>
2021-05-25 11:27:11 +0100

    fix(log-generator): `generate_batch()` produces malformed CSV file (#3)
    * fix(log-generator): `generate_batch()` produces malformed CSV file
    
    * Avoid writing CSV to FS

--- log-generator/generator.py
+++ log-generator/generator.py
@@ -14,6 +14,7 @@ Most of the behaviour can be controlled by tweaking global variables below. You'
 (Kinesis stream/S3 bucket) to your own.
 """
 
+import csv
 import io
 import json
 import os
@@ -228,12 +229,20 @@ def generate_batch(users, items):
 
     prev_hour = datetime.now().replace(microsecond=0, second=0, minute=0) - timedelta(hours=1)
     logs = generate_logs(users, prev_hour, lambda *a: line_generator_item_access(*a, items=items))
-    logs_as_csv_string = "\n".join((",".join(line) for line in logs))
 
-    s3 = boto3.client("s3")
-
-    with io.BytesIO(logs_as_csv_string.encode('utf-8')) as f:
-        s3.upload_fileobj(f, S3_BUCKET, S3_PREFIX + prev_hour.strftime("/%Y/%m/%d/%H.csv"))
+    with io.StringIO() as batch:
+        logs = map(json.loads, logs)
+        first, rest = next(logs), logs
+        writer = csv.DictWriter(batch, fieldnames=first.keys())
+        writer.writeheader()
+        writer.writerow(first)
+        writer.writerows(rest)
+        batch.flush()
+        boto3.client("s3").upload_fileobj(
+            io.BytesIO(batch.getvalue().encode()),
+            S3_BUCKET,
+            S3_PREFIX + prev_hour.strftime("/%Y/%m/%d/%H.csv")
+        )
 
 
 def main(mode, stream=None):
